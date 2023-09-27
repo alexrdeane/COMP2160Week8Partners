a26878c9af7ae76bc8941e5c6ab1d857bb4c5b8a
Alex Deane <alexrdeane@gmail.com>
2023-09-27 15:01:57 +1000

    playerInputs and semi player script

---
+++ COMP2160Week8Partners Project/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    private PlayerInput playerInputs;
+    private InputAction movement;
+
+    private void Awake()
+    {
+        playerInputs = new PlayerInput();
+        movement = playerInputs.playerActions.movement;
+    }
+
+    void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    void OnDisable()
+    {
+        movement.Disable();
+    }
+
+    void Update()
+    {
+        float movementX = movement.ReadValue<Vector2>().x;
+        float movementY = movement.ReadValue<Vector2>().y;
+
+        transform.Translate(movementX * movementSpeed * Time.deltaTime, movementY * movementSpeed * Time.deltaTime, 0, Space.Self);
+    }
+}
